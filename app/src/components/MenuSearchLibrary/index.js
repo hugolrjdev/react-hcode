d46e88b8c584914371cb90acb1a8892059ac3e06
hugolrjdev <fortalezaplanodesaude@gmail.com>
2022-07-29 16:18:43 -0300

    atualizando desafio

--- app/src/components/MenuSearchLibrary/index.js
+++ app/src/components/MenuSearchLibrary/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Home, Search, YLibrary } from "../Ultils/Icons";
 
+const mainMenuIcons = [
+  { icon: <Home />, label: "Início", link: "/home" },
+  { icon: <Search />, label: "Buscar", link: "/home" },
+  { icon: <YLibrary />, label: "Sua Biblioteca", link: "/home" },
+];
+
 export default function MenuSearchLibrary(props) {
   return (
     <ul className="menu-search-library">
-      <li className="inicio">
-        <Home />
-      </li>
-      <li className="search">
-        <Search />
-      </li>
-      <li className="playlist">
-        <YLibrary />
-      </li>
+      {mainMenuIcons.map((icon, index) => (
+        <li key={index}>
+          <a href={icon.link}>
+            {icon.icon}
+            <span>{icon.label}</span>
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
