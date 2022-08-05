1a6ede95d9e780f4f677a840ac588b5c6c245576
hugolrjdev <fortalezaplanodesaude@gmail.com>
2022-08-04 22:07:37 -0300

    footer

--- hcode-react-router/src/App.tsx
+++ hcode-react-router/src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo-porsche.png";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="#" className="logo" aria-label="Logo">
+          <img src={logo} alt="Logo da Porsche" />
         </a>
+        <nav className="menu">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Models</a>
+            </li>
+            <li>
+              <a href="#">Descubra</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <footer>
+        <div className="content">
+          <div className="copyright">
+            <p>
+              © 2022 Hcode Treinamentos Brasil Importadora de Veículos Ltda.
+              Indicações Legais. Política de Privacidade. Compliance Porsche.
+              Open Source Software Notice.
+            </p>
+            <p>
+              * As informações encontradas neste site referentes ao Porsche
+              Connect não se aplicam ao Brasil, já que o programa ainda não está
+              disponível no mercado brasileiro. Para mais informações sobre os
+              produtos disponíveis no Brasil entre em contato com o Porsche
+              Center de sua preferência.
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
